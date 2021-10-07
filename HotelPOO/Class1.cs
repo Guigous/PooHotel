71f98adf6e954170416a00213e35efa8978eca9a
Guigous <gui_1810@hotmail.com>
2021-10-07 03:19:54 -0300

    Add project files.

---
+++ HotelPOO/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelPOO
+{
+    class Hospede
+    {
+        public string Nome { get; set; }
+        public string Email{ get; set; }
+        //Encapsulamento
+        public Hospede(string nome , string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return Nome + " , " + Email ;
+        }
+    }
+}
---
+++ HotelPOO/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace HotelPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instaciamento da classe
+            Hospede[] vect = new Hospede[10];
+            //Input para gerar a matriz
+            Console.WriteLine("Quantos quartos serao alugados ?");
+            int n = int.Parse(Console.ReadLine());
+            
+            //Loop da Matriz
+            //Input e Processamento de dados
+            for (int i =1; i<=n; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Hospede # {i}");
+                Console.WriteLine("Nome");
+                string nome = Console.ReadLine();
+                Console.WriteLine("Email");
+                string email = Console.ReadLine();
+                Console.WriteLine("Quarto :");
+                int quarto = int.Parse(Console.ReadLine());
+                vect[quarto] = new Hospede(nome, email);
+            }
+            //Output de dados
+            Console.WriteLine();
+            Console.WriteLine("Quartos Ocupados: ");
+            for (int i = 0; i<10; i++)
+            {
+               if (vect[i] != null)
+                {
+                    Console.WriteLine(i + ": " + vect[i]);
+                }
+            }
+        }
+    }
+}
